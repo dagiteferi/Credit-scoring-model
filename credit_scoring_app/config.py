d14c8cd7b84b47af8b8ec08e4e1d612ac477883c
Dagmawi <dagiteferi2011@gmail.com>
2025-03-10 13:58:41 +0300

    Update model path in config and add init file for models directory

--- credit_scoring_app/config.py
+++ credit_scoring_app/config.py
@@ -1,9 +1,9 @@
-# config.py
+
 import logging
 import os
 
-# Path to the saved model
-MODEL_PATH = os.path.join(os.path.dirname(__file__), "models", "RandomForest_best_model.pkl")
+# Path to the saved model 
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "..", "models", "RandomForest_best_model.pkl")
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
